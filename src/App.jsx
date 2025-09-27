1f2a63652b7431a254bc3ace6039b65e96f12dde
Sufiyan <skhan367227@gmail.com>
2025-09-27 18:16:40 +0530

    Fix: added default export in App.jsx to resolveimport error

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 // App.jsx (Updated - Footer on all pages)
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './Navbar'
-import Herosection from './Herosection'
-import Cards from './Cards'
-import Ourclient from './Ourclient'
-import Footer from './Footer'
-import About from './About'
-import Service from './Service'
-import Teampage from './Teampage'
-import Contact from './Contact'
+import Navbar from './Components/Navbar'
+import Herosection from './Components/Herosection'
+import Cards from './Components/Cards'
+import Ourclient from './Components/Ourclient'
+import Footer from './Components/Footer'
+import About from './Components/About'
+import Service from './Components/Service'
+import Teampage from './Components/Teampage'
+import Contact from './Components/Contact'
 
 const App = () => {
   return (
